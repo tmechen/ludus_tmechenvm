824f747177ee4851f089a7d1436f0ede68a5b492
tmechen <mail@tmechen.me>
2025-03-07 23:32:13 +0100

    add path and install

--- files/install_zimmermann.ps1
+++ files/install_zimmermann.ps1
@@ -1,6 +1,7 @@
 $installPath = "C:\Tools\EricZimmermann"
 $zipUrl = "https://download.ericzimmermanstools.com/Get-ZimmermanTools.zip"
 $zipFile = "$installPath\ZimmermanTools.zip"
+$installerScript = "$installPath\Get-ZimmermanTools.ps1"
 
 if (!(Test-Path -Path $installPath)) {
     New-Item -ItemType Directory -Path $installPath -Force | Out-Null
@@ -8,4 +9,13 @@ if (!(Test-Path -Path $installPath)) {
 
 Invoke-WebRequest -Uri $zipUrl -OutFile $zipFile
 Expand-Archive -Path $zipFile -DestinationPath $installPath -Force
+if (Test-Path -Path $installerScript) {
+    PowerShell -ExecutionPolicy Bypass -File $installerScript
+}
 Remove-Item $zipFile -Force
+Remove-Item $installerScript -Force
+$envPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
+if ($envPath -notlike "*$net6Path*") {
+    $newPath = "$envPath;$net6Path"
+    [System.Environment]::SetEnvironmentVariable("Path", $newPath, [System.EnvironmentVariableTarget]::Machine)
+}
