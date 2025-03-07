a6fff3ac7312cf819163dec7bd155a9f3ee78c32
tmechen <mail@tmechen.me>
2025-03-07 23:50:38 +0100

    remove deletion

--- files/install_zimmermann.ps1
+++ files/install_zimmermann.ps1
@@ -9,11 +9,7 @@ if (!(Test-Path -Path $installPath)) {
 
 Invoke-WebRequest -Uri $zipUrl -OutFile $zipFile
 Expand-Archive -Path $zipFile -DestinationPath $installPath -Force
-if (Test-Path -Path $installerScript) {
-    PowerShell -ExecutionPolicy Bypass -File $installerScript
-}
-Remove-Item $zipFile -Force
-Remove-Item $installerScript -Force
+PowerShell -ExecutionPolicy Bypass -File $installerScript
 $envPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
 if ($envPath -notlike "*$net6Path*") {
     $newPath = "$envPath;$net6Path"
